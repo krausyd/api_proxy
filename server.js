9996b18ff79d8ff3264bd9c4aac89c015ca04250
karina <kguerrerofernandez@instructors.2u.com>
2023-05-22 18:46:28 -0700

    Create endpoint to get a token from an api key

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const port=8000;    
+const keys = require("./apiKeys.json");
+
+app.get('/', (req, res) => {
+    res.send('This is the api proxy');
+});
+
+
+app.get('/token/:apikey', async (req, res) => {
+    console.log(`Getting token for ${req.params.apikey}`);
+    const dataKey = keys[req.params.apikey];
+    if (dataKey) {
+        const response = await fetch(dataKey.url, {
+            method: 'POST',
+            body: JSON.stringify({
+                "grant_type": "client_credentials",
+                "client_id": dataKey.client_id,
+                "client_secret": dataKey.client_secret,
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            },
+        });
+        if (response.status === 200) {
+            const data = await response.json();
+            console.log('Returning the token to the user');
+            res.status(200).send(data);
+        } else {
+            console.log(`Api returned ${response.status}, ${response.statusText}`);
+            res.status(response.status).send(response.statusText);
+        }
+    } else {
+        res.status(404).json({"message":"api key not found"});
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Api proxy ready and listening on port ${port}`);
+})
